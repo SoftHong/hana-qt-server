2df0dee7147e5bffd597c8a938dd9bf41fa0ba2d
seongho <hsh3592@gmail.com>
2017-12-23 01:29:59 +0900

    Edit admin table

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
-
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'title', 'published_date']
+    list_display = ['author', 'title', 'published_date', ]
     list_display_links = ['title']
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'message']
+    list_display_links = ['message']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
