38fbfc6eb3f736d333beab5c3ade198e43dfe8da
seongho <hsh3592@gmail.com>
2018-02-18 00:12:23 +0900

    use timezone

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,13 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
